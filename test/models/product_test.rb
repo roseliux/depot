5425b1dcf41c2b3933a6cc2e8655eb179b2b942c
roseliux <roxdurazo@gmail.com>
2017-01-09 23:45:02 -0700

    Validate length of title.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -11,7 +11,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title: "Hello",
+    product = Product.new(title: "Hello akdjfakldjf",
                           description: 'test',
                           image_url: 'xyz.jpg')
     product.price = -1
@@ -23,7 +23,46 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = 1
     assert product.valid?
-
-
   end
+
+  def new_product(image_url)
+    Product.new(title: "Hello asdfasdfasdfadsf",
+                description: 'test',
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid."
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid."
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'test',
+                          price: 1,
+                          image_url: 'img.png')
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                 product.errors[:title]
+  end
+
+  test "check that the title is at least ten characters" do
+    product = Product.new(title: '123456789',
+                          description: 'test',
+                          price: 1,
+                          image_url: 'img.png')
+    assert product.invalid?, "length is #{product.title.length} must be greater than or equal to 10."
+    assert_equal ["10 characters is the minimum allowed."],
+                 product.errors[:title]
+  end
+
 end
