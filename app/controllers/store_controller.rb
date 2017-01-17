8c65af2a8842dce642383c58be8cfee57e038d4c
roseliux <roxdurazo@gmail.com>
2017-01-17 11:14:01 -0700

    Add counter to the store index, reset when something is added to the cart.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+  before_action :set_counter, only: :index
   def index
     @products = Product.order(:title)
+    session[:counter] += 1
+  end
+
+  private
+  def set_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
   end
 end
