bb59b4a84b1a09bdc24414e56fa4de72519cb797
roseliux <roxdurazo@gmail.com>
2017-01-22 21:48:40 -0700

    Move cart to side bar and Update with ajax.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   before_action :set_counter, only: :index
   def index
     @products = Product.order(:title)
