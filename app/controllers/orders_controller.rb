f08ea2f17acbd09bb27e93339ce72fcd69749e86
roseliux <roxdurazo@gmail.com>
2017-03-17 17:38:15 -0700

    Capturing an Order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,14 +26,19 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_index_url, notice:
+          'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.json { render json: @order.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -76,6 +81,8 @@ class OrdersController < ApplicationController
   end
 
   def ensure_cart_isnt_empty
-    redirect_to store_index_url, notice: 'Your cart is empty' if @cart.line_items.empty?
+    if @cart.line_items.empty?
+      redirect_to store_index_url, notice: 'Your cart is empty'
+    end
   end
 end
