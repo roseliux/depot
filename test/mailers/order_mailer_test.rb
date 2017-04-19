fbeef00707c9a97bfbf6afbf5268f2a74e90d74b
roseliux <roxdurazo@gmail.com>
2017-04-18 22:39:47 -0700

    Integration test with 0 errors.

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -14,7 +14,7 @@ class OrderMailerTest < ActionMailer::TestCase
     assert_equal 'Pragmatic Store Order Shipped', mail.subject
     assert_equal ['dave@example.org'], mail.to
     assert_equal ['depot@example.com'], mail.from
-    assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/,
+    assert_match '<td> Programming Ruby 1.9 </td>',
                  mail.body.encoded
   end
 
