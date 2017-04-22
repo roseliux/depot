b39cbe8b286568dd055711e3f5e50d92a0eb779d
roseliux <roxdurazo@gmail.com>
2017-04-21 17:11:31 -0700

    Limiting access

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,16 +1,17 @@
 class StoreController < ApplicationController
   include CurrentCart
+  skip_before_action :authorize
   before_action :set_cart
   before_action :set_counter, only: :index
+
   def index
     @products = Product.order(:title)
     session[:counter] += 1
   end
 
   private
+
   def set_counter
-    if session[:counter].nil?
-      session[:counter] = 0
-    end
+    session[:counter] = 0 if session[:counter].nil?
   end
 end
